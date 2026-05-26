6c7c7b83ebe408dc8af49f4148f62cb4a3371f90
tahsinaltabib <nabilarah33@gmail.com>
2026-05-26 08:14:10 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// const tabibDetails={
+//     name: "Tahsin Al Tabib",
+//     age: 21,
+//     isCoder: true,
+//     Skills: ["Javascript, React Js, Node Js, MongoDB"]
+// };
+
+// console.log(tabibDetails.name);
+// console.log(tabibDetails.Skills);
+
+
+let tabibAge= 21;
+let tabibSkill= "NodeJs";
+
+console.log(tabibSkill=== "NodeJs" );
+
+
